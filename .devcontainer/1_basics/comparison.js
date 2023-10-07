ba895a3699de19ae62a061a9a36bc4e076cb816a
Harsh-620 <89959976+Harsh-620@users.noreply.github.com>
2023-10-07 18:46:13 +0000

    studied about different operations and conversions

---
+++ .devcontainer/1_basics/comparison.js
@@ -0,0 +1,9 @@
+console.log(typeof "02")
+//typescript don't allow comparion of two different datatypes values
+/* NULL*/
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//Avoid using comparison operator with null and undefined
+//=== helps in strict check (check with both value and datatype)
---
+++ .devcontainer/1_basics/conversionoperation.js
@@ -0,0 +1,20 @@
+let value=3;
+let negvalue=-value //it converts the original value to negative.
+//console.log(negvalue);
+//concatenation
+let a1="harsh"
+let a2="ranjan"
+//console.log(a1+a2) //harshranjan
+
+/*conversion and concatenation*/
+//console.log("1"+2)//12
+//console.log(1+2+"3")//33
+
+/*postfix and prefix increament*/
+let x = 3;
+const y = x++; //prefix
+console.log(x) //4
+console.log(y) //3
+const z=++x
+console.log(x) //5
+console.log(z) //5
